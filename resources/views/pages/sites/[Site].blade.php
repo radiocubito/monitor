1c255a6eb979bffa2bf08df07b694d6859779d72
Oliver Servín <oliverds@users.noreply.github.com>
2023-10-15 18:16:23 -0600

    wip

--- resources/views/pages/sites/[Site].blade.php
+++ resources/views/pages/sites/[Site].blade.php
@@ -51,7 +51,7 @@ $updateEmails = function (Site $site) {
     <x-slot name="title">
         <div class="flex items-center">
             <span class="text-gray-800 w-full text-lg font-medium lowercase">
-                {{ $site->domain }}
+                Endpoints
             </span>
         </div>
     </x-slot>
@@ -81,7 +81,7 @@ $updateEmails = function (Site $site) {
                     <li>
                         <a href="#" class="text-gray-600 hover:text-indigo-500 flex items-center gap-x-2.5 rounded-xl px-2.5 py-2.5" x-state:on="Current" x-state:off="Default" x-state-description="Current: &quot;bg-gray-50 text-indigo-600&quot;, Default: &quot;text-gray-700 hover:text-indigo-600 hover:bg-gray-50&quot;">
                             <svg class="h-[1.25rem] w-[1.25rem]" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.5 3h.5v0c1.77 0 3.32 1.18 3.78 2.89l1.24 4.56c.14.51.21.77.26 1.02 .37 1.65.39 3.37.05 5.04 -.06.25-.12.51-.25 1.03v0c-.16.61-.24.92-.34 1.19 -.66 1.75-2.25 3-4.12 3.21 -.29.03-.61.03-1.24.03H8.52c-.64 0-.96 0-1.24-.04 -1.87-.22-3.46-1.46-4.12-3.22 -.1-.27-.18-.58-.34-1.2v0c-.13-.52-.2-.78-.25-1.04 -.34-1.67-.33-3.39.05-5.05 .05-.26.12-.52.26-1.03l1.24-4.57c.46-1.71 2.01-2.9 3.78-2.9v0h.5m-6 11h2.78c.62 0 .93 0 1.22.08 .25.07.48.2.68.36 .22.18.4.44.75.97l.1.15c.34.52.52.78.75.97 .2.16.43.29.68.36 .28.08.59.08 1.22.08h2.57c.62 0 .93 0 1.22-.09 .25-.08.48-.21.68-.37 .22-.19.4-.45.75-.98l.1-.15c.34-.53.52-.79.75-.98 .2-.17.43-.3.68-.37 .28-.09.59-.09 1.22-.09h2.78m-9.5-3l3-3m-3 3l-3-3m2.99 3v-9"></path>
+                                <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19.36 2H4.62c-.91 0-1.64.73-1.64 1.63 0 2.11 1.01 4.09 2.73 5.32l1.92 1.37h0c.64.45.96.68 1.22.95 .53.55.89 1.26 1.04 2.02 .06.36.06.76.06 1.55v5.13c0 1.1.89 2 2 2 1.1 0 2-.9 2-2v-5.14c0-.79 0-1.19.06-1.56 .14-.77.5-1.47 1.04-2.03 .25-.28.58-.5 1.22-.96l1.92-1.38c1.71-1.23 2.73-3.21 2.73-5.33 0-.91-.74-1.64-1.64-1.64Z"></path>
                             </svg>
                             <span class="align-middle font-mono text-sm">charts</span>
                         </a>
@@ -121,33 +121,39 @@ $updateEmails = function (Site $site) {
 
     <div class="p-6 space-y-5 bg-gray-50 h-full">
         @volt('endpoint-list')
-            <table class="border-collapse table-auto w-full">
-                <thead>
-                    <tr>
-                        <th class="text-left">Ubicación</th>
-                        <th class="text-left">Frecuencia</th>
-                        <th class="text-left">Revisado por última vez</th>
-                        <th class="text-left">Último estado</th>
-                        <th class="text-left">Uptime</th>
-                        <th class="text-left"></th>
-                        <th class="text-left"></th>
-                    </tr>
-                </thead>
-                <tbody>
+            <div class="mx-auto flex w-full flex-col space-y-2.5 px-4 pt-4 lg:max-w-3xl">
+                <div class="bg-white shadow overflow-hidden sm:rounded-xl p-4">
+                    <div class="grid grid-cols-5 gap-x-5">
+                        <div class="truncate col-span-2">
+                            <span class="text-gray-900 truncate font-medium">Endpoint</span>
+                        </div>
+                        <div>
+                            <span class="text-gray-800 text-sm font-medium">Última revisión</span>
+                        </div>
+                        <div>
+                            <span class="text-gray-800 text-sm font-medium">Último estado</span>
+                        </div>
+                        <div>
+                            <span class="text-gray-800 text-sm font-medium">Disponibilidad</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="bg-white shadow overflow-hidden sm:rounded-xl">
                     @foreach($endpoints as $endpoint)
-                        <tr wire:key="{{ $endpoint->id }}">
-                            <td>
-                                <a href="{{ route('endpoints.show', ['endpoint' => $endpoint]) }}">{{ $endpoint->location }}</a>
-                            </td>
-                            <td>{{ $endpoint->frequency_label }}</td>
-                            <td>
+                        <a href="{{ route('endpoints.show', ['endpoint' => $endpoint]) }}" class="grid grid-cols-5 gap-x-5 p-4" wire:key="{{ $endpoint->id }}">
+                            <div class="col-span-2 ">
+                                <div class="text-gray-800 text-sm font-medium truncate">{{ $endpoint->location }}</div>
+                                <div class="text-gray-600 text-sm">{{ $endpoint->frequency_label }}</div>
+                            </div>
+                            <div class="text-gray-600 text-sm">
                                 @if($endpoint->check)
                                     {{ $endpoint->check->created_at->toDateTimeString() }}
                                 @else
                                     -
                                 @endif
-                            </td>
-                            <td>
+                            </div>
+                            <div class="text-gray-600 text-sm flex items-center">
                                 @if($endpoint->check)
                                     <span @class([
                                         'text-green-600' => $endpoint->check->isSuccessful(),
@@ -158,30 +164,22 @@ $updateEmails = function (Site $site) {
                                 @else
                                     -
                                 @endif
-                            </td>
-                            <td>
+                            </div>
+                            <div class="text-gray-600 text-sm flex items-center">
                                 @if ($endpoint->uptimePercentage() !== null)
                                     {{ $endpoint->uptimePercentage() }}%
                                 @else
                                     -
                                 @endif
-                            </td>
-                            <td>
-                                <a href="{{ route('endpoint.edit', ['endpoint' => $endpoint]) }}">Editar</a>
-                            </td>
-                            <td>
-                                <button type="button" wire:click="delete({{ $endpoint->id }})">
-                                    Eliminar
-                                </button>
-                            </td>
-                        </tr>
+                            </div>
+                        </a>
                     @endforeach
-                </tbody>
-            </table>
+                </div>
+            </div>
         @endvolt
 
         @volt('emails')
-            <form wire:submit="updateEmails({{ $site->id }})" class="space-y-2 mt-5">
+            <form wire:submit="updateEmails({{ $site->id }})" class="space-y-2 mt-5 hidden">
                 <h2>Emails para notificaciones:</h2>
                 <input wire:model="emails" name="emails" type="text" class="block">
                 @error('emails')
