67688523f11caf35bd5d53b2613846e9e07bd5a3
Oliver Servín <oliverds@users.noreply.github.com>
2023-10-17 10:30:37 -0600

    wip

--- lang/es/passwords.php
+++ lang/es/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset.',
-    'sent' => 'We have emailed your password reset link.',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'reset' => 'Tu contraseña ha sido restablecida!',
+    'sent' => '¡Te hemos enviado por correo electrónico el enlace para restablecer tu contraseña!',
+    'throttled' => 'Por favor espera antes de intentar de nuevo.',
+    'token' => 'El token de restablecimiento de contraseña es inválido.',
+    'user' => 'No encontramos ningún usuario con ese correo electrónico.',
 
 ];
