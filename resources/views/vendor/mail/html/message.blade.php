8d2f4d17722c726ef179df295a9d4aa31c96caf9
Oliver Servín <oliverds@users.noreply.github.com>
2023-10-24 11:19:13 -0600

    wip

--- resources/views/vendor/mail/html/message.blade.php
+++ resources/views/vendor/mail/html/message.blade.php
@@ -1,11 +1,4 @@
 <x-mail::layout>
-{{-- Header --}}
-<x-slot:header>
-<x-mail::header :url="config('app.url')">
-{{ config('app.name') }}
-</x-mail::header>
-</x-slot:header>
-
 {{-- Body --}}
 {{ $slot }}
 
@@ -17,11 +10,4 @@
 </x-mail::subcopy>
 </x-slot:subcopy>
 @endisset
-
-{{-- Footer --}}
-<x-slot:footer>
-<x-mail::footer>
-© {{ date('Y') }} {{ config('app.name') }}. @lang('All rights reserved.')
-</x-mail::footer>
-</x-slot:footer>
 </x-mail::layout>
