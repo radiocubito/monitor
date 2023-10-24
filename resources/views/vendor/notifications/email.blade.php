04c9a77dd5830e81a6aa3832cde1d49c6d405bae
Oliver Servín <oliverds@users.noreply.github.com>
2023-10-24 11:43:17 -0600

    wip

--- resources/views/vendor/notifications/email.blade.php
+++ resources/views/vendor/notifications/email.blade.php
@@ -4,9 +4,9 @@
 {{ $greeting }}
 @else
 @if ($level === 'error')
-@lang('Whoops!')
+¡Ups!
 @else
-@lang('Hello!')
+Hola
 @endif
 @endif
 
@@ -39,7 +39,7 @@
 @if (! empty($salutation))
 {{ $salutation }}
 @else
-@lang('Regards'),<br>
+Saludos,<br>
 {{ config('app.name') }}
 @endif
 
@@ -47,8 +47,8 @@
 @isset($actionText)
 <x-slot:subcopy>
 @lang(
-    "If you're having trouble clicking the \":actionText\" button, copy and paste the URL below\n".
-    'into your web browser:',
+    "Si tienes problemas para hacer clic en el enlace \":actionText\", copia y pega la siguiente URL\n".
+    'en tu navegador web:',
     [
         'actionText' => $actionText,
     ]
